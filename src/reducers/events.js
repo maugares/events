3d4348354578c0aeb84d2ca0529917f29cc9ce49
maugares <mauro.garciaesteban@gmail.com>
2019-04-18 10:20:36 +0200

    steps before creating the event reducer

---
+++ src/reducers/events.js
@@ -0,0 +1,2 @@
+/* Create a reducer in reducers/events.js that responds to the EVENTS_FETCHED action by replacing the state with action.events. The default state should be null. Add the events reducer to the reducers/index.js. */
+
