0d46a73d65fb94afcfa9a43c7b550ea94a80d14f
maugares <mauro.garciaesteban@gmail.com>
2019-04-18 12:20:50 +0200

    render events and link them

--- src/components/EventsList.js
+++ src/components/EventsList.js
@@ -1,7 +1,7 @@
 /* 
   OK 1. Create the EventsList 
-  -- 2. that renders an <li> in an <ul> for every event
-  -- 3. Inside the <li> a <Link> (from react-router-dom) should        be used
+  OK 2. that renders an <li> in an <ul> for every event
+  OK 3. Inside the <li> a <Link> (from react-router-dom) should        be used
   -- 4. to link to another page located on /events/:id. 
   -- 5. When there are no events yet, just display the text            'Loading...'. 
 */
@@ -12,22 +12,20 @@ import { Link } from 'react-router-dom'
 export default class EventsList extends Component {
   renderEventList = (events) => {
     if (!events) return 'Loading...'
-    else events.map(event =>
-      <ul>
-        <li>
-          <Link to="/events/:id">{event}</Link>
-        </li>
-      </ul>
+    return events.map(event => {
+
+      return <Link to={`/events/${event.id}`} key={event.name}>
+        <li>{event.name}</li>
+      </Link>
+    }
     )
   }
 
   render() {
     const { events } = this.props
-    console.log('Events from EventsList:', events)
-    console.log('renderEventList():', this.renderEventList())
     return (
       <div>
-
+        <ul>{this.renderEventList(events)}</ul>
       </div>
     )
   }
